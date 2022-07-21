a8577bf989592f635820b0b83b787825e3784dd8
Svetlana Murdoch <66595456+Svetkachalina@users.noreply.github.com>
2022-07-21 11:42:16 +0300

    Add files via upload

---
+++ js/module/menu.js
@@ -0,0 +1,7 @@
+const headerNavBtn = document.querySelector('.header__nav-btn');
+const headerNavigation = document.querySelector('.header__navigation');
+
+headerNavBtn.addEventListener('click',  () => {
+    headerNavigation.classList.toggle('header__navigation_open');
+});
+
